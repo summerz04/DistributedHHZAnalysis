3434bf8ad254b00f6a228443e9f072c90d4a8586
summerz04 <summerzhao2004@gmail.com>
2026-04-01 12:31:02 +0100

    Defining analysis functions for workers

---
+++ shared/analysis_functions.py
@@ -0,0 +1,80 @@
+# analysis functions that the master.py and worker.py scripts need
+# directly taken from HZZ jupyter notebook 
+
+import numpy as np
+import awkward as ak
+import vector 
+
+# unit definitions, as stored in the data files
+MeV = 0.001
+GeV = 1.0
+
+# data size for analysis 
+lumi = 36.6 # fb-1 # data size of the full release
+
+# luminosity of data_D
+#lumi = 0.105 #Luminosity of periodD in data 15 is 0.105 fb-1
+
+fraction = 1.0 # reduce this is if you want the code to run quicker
+
+# define what variables are important to our analysis
+variables = ['lep_pt','lep_eta','lep_phi','lep_e','lep_charge','lep_type','trigE','trigM','lep_isTrigMatched',
+            'lep_isLooseID','lep_isMediumID','lep_isLooseIso','lep_type']
+
+weight_variables = ["filteff","kfac","xsec","mcWeight","ScaleFactor_PILEUP", "ScaleFactor_ELE", "ScaleFactor_MUON", "ScaleFactor_LepTRIGGER"]
+
+# For convenient naming and identification purposes, we define a dictionary which stores all the important names of the samples we want to pull from the database.
+defs = {
+    r'Data':{'dids':['data']},
+    r'Background $Z,t\bar{t},t\bar{t}+V,VVV$':{'dids': [410470,410155,410218,
+                                                        410219,412043,364243,
+                                                        364242,364246,364248,
+                                                        700320,700321,700322,
+                                                        700323,700324,700325], 'color': "#6b59d3" }, # purple
+    r'Background $ZZ^{*}$':     {'dids': [700600],'color': "#ff0000" },# red
+    r'Signal ($m_H$ = 125 GeV)':  {'dids': [345060, 346228, 346310, 346311, 346312,
+                                          346340, 346341, 346342],'color': "#00cdff" }, # light blue 
+        }
+
+
+# cut functions 
+# Cut lepton type (electron type is 11,  muon type is 13)
+def cut_lep_type(lep_type):
+    sum_lep_type = lep_type[:, 0] + lep_type[:, 1] + lep_type[:, 2] + lep_type[:, 3]
+    lep_type_cut_bool = (sum_lep_type != 44) & (sum_lep_type != 48) & (sum_lep_type != 52)
+    return lep_type_cut_bool # True means we should remove this entry (lepton type does not match)
+
+# Cut lepton charge
+def cut_lep_charge(lep_charge):
+    # first lepton in each event is [:, 0], 2nd lepton is [:, 1] etc
+    sum_lep_charge = lep_charge[:, 0] + lep_charge[:, 1] + lep_charge[:, 2] + lep_charge[:, 3] != 0
+    return sum_lep_charge # True means we should remove this entry (sum of lepton charges is not equal to 0)
+
+# Calculate invariant mass of the 4-lepton state
+# [:, i] selects the i-th lepton in each event
+def calc_mass(lep_pt, lep_eta, lep_phi, lep_e):
+    p4 = vector.zip({"pt": lep_pt, "eta": lep_eta, "phi": lep_phi, "E": lep_e})
+    invariant_mass = (p4[:, 0] + p4[:, 1] + p4[:, 2] + p4[:, 3]).M # .M calculates the invariant mass
+    return invariant_mass
+
+
+def cut_trig_match(lep_trigmatch):
+    trigmatch = lep_trigmatch
+    cut1 = ak.sum(trigmatch, axis=1) >= 1
+    return cut1
+
+def cut_trig(trigE,trigM):
+    return trigE | trigM
+
+
+def ID_iso_cut(IDel,IDmu,isoel,isomu,pid):
+    thispid = pid
+    return (ak.sum(((thispid == 13) & IDmu & isomu) | ((thispid == 11) & IDel & isoel), axis=1) == 4)
+
+
+# calculation for final histogram
+def calc_weight(weight_variables, events):
+    total_weight = lumi * 1000 / events["sum_of_weights"]
+    for variable in weight_variables:
+        total_weight = total_weight * abs(events[variable])
+    return total_weight
