698f7df84820aea7e4355b3831bb1c68615180db
summerz04 <summerzhao2004@gmail.com>
2026-04-01 13:19:42 +0100

    clearing master.py

--- for_Master/master.py
+++ for_Master/master.py
@@ -1,12 +1,1 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-print('Hello from container!')
-
-x = np.linspace(0, 100, 100)
-y = x**2
-plt.plot(x, y, label="$y=x^2$")
-plt.legend()
-plt.savefig("test.png")
-
-print('Bye bye container!')
+# master file should read data, send data to workers, and then collect results back to plot the final histogram
